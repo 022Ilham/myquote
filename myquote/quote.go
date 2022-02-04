1d73d4e553bc6e9802bdb7e6639ddbb8505e5cba
022Ilham <02ilham@gmail.com>
2022-02-04 12:14:44 +0100

    first commit

---
+++ myquote/quote.go
@@ -0,0 +1,17 @@
+package myquote
+
+import "rsc.io/quote"
+
+func Glass() string {
+	return quote.Glass()
+}
+func Go() string {
+	return quote.Go()
+}
+func Hello() string {
+	return quote.Hello()
+}
+
+func Opt() string {
+	return quote.Opt()
+}
